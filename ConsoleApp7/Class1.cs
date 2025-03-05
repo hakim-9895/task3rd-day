7e24b34d843ff163a19b1d9c45eb614b36a7f754
hakim-9895 <abdulhakim9895@gmail.com>
2025-03-05 19:59:33 +0530

    Add project files.

---
+++ ConsoleApp7/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp7
+{
+    internal class Services
+    {
+        private string connectionstring;
+        public Services(string conection) { 
+            connectionstring= conection;
+        
+        }
+        public void insertstudent()
+        {
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("Invertprocedure", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name","hafiz");
+                    cmd.Parameters.AddWithValue("@age", 2);
+                    cmd.ExecuteNonQuery();
+
+                }
+                
+
+                
+            }
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Services services = new Services("Server=LAPTOP-P8PNFQFL;Database=Students;Integrated Security=True;TrustServerCertificate=True;");
+            services.insertstudent();
+        }
+       
+    }
+
+}
